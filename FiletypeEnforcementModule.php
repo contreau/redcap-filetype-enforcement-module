efb8990244263b1152c03933cc507c625512b78f
contreau <72581161+contreau@users.noreply.github.com>
2026-03-12 12:57:20 -0400

    init

---
+++ FiletypeEnforcementModule.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace ExternalModules\FiletypeEnforcementModule;
+
+// These 2 lines should always be included and be the same in every (advanced) module
+use ExternalModules\AbstractExternalModule;
+use ExternalModules\ExternalModules;
+
+class FiletypeEnforcementModule extends AbstractExternalModule {}
