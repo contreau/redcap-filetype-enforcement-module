5a2bb8691d6f4164a70cc99f3327b094e26c037d
contreau <72581161+contreau@users.noreply.github.com>
2026-04-16 14:00:16 -0400

    feat: at-a-glance filetypes in instrument editor

--- js/editor/instrument_editor.js
+++ js/editor/instrument_editor.js
@@ -1,4 +1,5 @@
 import { getModule } from "../shared/utils.js";
+import { observeFieldPreviewRerender } from "../shared/filetypes_glance.js";
 import { observeFieldEditorDialog } from "../shared/field_editor_dialog.js";
 import { observeNetwork } from "../shared/network_observer.js";
 import { FiletypeCheckboxesComponent } from "../shared/FiletypeCheckboxesComponent.js";
@@ -7,15 +8,18 @@ import { FiletypeCheckboxesComponent } from "../shared/FiletypeCheckboxesCompone
  * Runs on the instrument editor page.
  */
 (() => {
-  getModule()
-    .ajax("update_instrument_name")
-    .then((res) => {
-      console.log(res);
-    });
+  const module = getModule();
+
+  module.ajax("update_instrument_name").then((res) => {
+    window.location.hostname === "localhost" ? console.log(res) : null;
+  });
+
+  // Mutation Observer that shows filetypes at-a-glance per file field.
+  observeFieldPreviewRerender(module);
 
   // Mutation Observer for field editor dialog.
-  observeFieldEditorDialog(FiletypeCheckboxesComponent, getModule());
+  observeFieldEditorDialog(FiletypeCheckboxesComponent, module);
 
   // Network Observer for handling field deletion.
-  observeNetwork("delete_field.php", getModule());
+  observeNetwork("delete_field.php", module);
 })();
---
+++ js/shared/filetypes_glance.js
@@ -0,0 +1,60 @@
+/**
+ * Creates observer to append/update the at-a-glance filetypes per file field.
+ * @param {*} module JS Module Object
+ */
+export async function observeFieldPreviewRerender(module) {
+  displayFiletypesAtGlance(module); // initial appending of filetype info
+  const parent = document.querySelector("div#draggablecontainer_parent");
+  const observer = new MutationObserver((mutationList) => {
+    for (const mutationRecord of mutationList) {
+      for (const node of mutationRecord.addedNodes) {
+        if (node.nodeType === Node.ELEMENT_NODE && node.hasAttribute("sq_id")) {
+          // Handles rendering behavior when a field is added or edited (only that field rerenders)
+          const fieldname = node.getAttribute("sq_id");
+          displaySingleFieldFiletypes(module, fieldname);
+        } else if (node.nodeType === Node.ELEMENT_NODE && node.id === "draggablecontainer") {
+          // Handles rerendering behavior when a field is deleted (all of div#draggablecontainer is rerendered)
+          displayFiletypesAtGlance(module);
+        }
+      }
+    }
+  });
+  observer.observe(parent, { childList: true, subtree: true });
+}
+
+/**
+ * Displays the enforced filetypes per file field on the instrument editor page; enables knowing active filetypes without having to open a field editor dialog.
+ * To be run on in instrument_editor.js as well as on field dialog save.
+ * @param {*} module JS Module Object
+ */
+async function displayFiletypesAtGlance(module) {
+  const filefieldSettings = await module.ajax("get_filefield_settings");
+  for (const fieldname of Object.keys(filefieldSettings)) {
+    createFiletypesLabel(fieldname, filefieldSettings);
+  }
+}
+
+/**
+ * Appends the at-a-glance file types label for a single file field.
+ * @param {*} module JS Module Object
+ * @param {string} fieldname
+ */
+async function displaySingleFieldFiletypes(module, fieldname) {
+  const filefieldSettings = await module.ajax("get_filefield_settings");
+  createFiletypesLabel(fieldname, filefieldSettings);
+}
+
+/**
+ * Helper for generating the at-a-glance file types label.
+ * @param {string} fieldname
+ * @param {string[]} filefieldSettings
+ */
+function createFiletypesLabel(fieldname, filefieldSettings) {
+  const elementTarget = document.querySelector(`div[data-mlm-field="${fieldname}"]`);
+  const filetypesLabel = document.createElement("p");
+  filetypesLabel.style.fontSize = "13px";
+  filetypesLabel.style.fontWeight = "500";
+  filetypesLabel.style.color = "#515151";
+  filetypesLabel.innerHTML = `Accepted: <strong>${filefieldSettings[fieldname]["extensions"].join(", ")}</strong>`;
+  elementTarget.after(filetypesLabel);
+}
